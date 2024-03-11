24e8352cfa9511f97363f8a4a2b8dfcd64168951
Macgarci <melanieadria12@gmail.com>
2024-03-11 16:47:24 -0600

    añadiendo comentarios

--- src/main/java/consoletask/classes/Main.java
+++ src/main/java/consoletask/classes/Main.java
@@ -7,22 +7,21 @@ import consoletask.utils.Validaciones;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Validaciones validacion = new Validaciones();
+        Scanner scanner = new Scanner(System.in); 
+        //crear un objeto de la calse Validaciones para validar la entrada del usuario
+        Validaciones validacion = new Validaciones(); 
         int opcion;
-        
+        //crear un nuevo nodo para almacenar las listas
         Nodo lista = new Nodo();
         
-        //Usuarios
+        //Crear instancias de usuario un administrador y un usuario
         Usuario admin = new Usuario("Admin", "admin123", RolUsuarioEnum.ADMIN.getId(), lista);
         Usuario usuario = new Usuario("Usuario", "user123", RolUsuarioEnum.USER.getId(), lista);
         
-        //Menú admin
+        //crear instancias de menu para el administrador y el usuario
         menu menuAdmin = new menu(admin, usuario);//Recibe el otro parametro ya que a este se le va copiar lo mismo que el otro usuario
-
-        //Menú usuario
         menu menuUsuario = new menu(usuario, admin);
-
+//varible para controlar la salida del bucle 
         boolean salida = false;
         while (!salida) {
             // Solicitar al usuario que elija el tipo de usuario
@@ -37,21 +36,21 @@ public class Main {
             // Crear una instancia de Menu según la opción seleccionada
             switch (opcion) {
                 case 1:
-                    menuAdmin.mostrarMenu();
+                    menuAdmin.mostrarMenu(); //mostrar el menu para el administrador
                     break;
                 case 2:
-                    menuUsuario.mostrarMenu();
+                    menuUsuario.mostrarMenu(); //mostrar menu para el usuario
                     break;
                 case 3:
-                    salida = true;
+                    salida = true; //establecer la variable de salida en verdadero para salir del bucle
                     break;
                 default:
-                    System.out.println("Opción inválida. Saliendo del programa...");
+                    System.out.println("Opción inválida. Saliendo del programa..."); // mensaje de error para opciones invalidas
                     break;
             }
         }
 
-        scanner.close();
+        scanner.close(); //cerrar objeto scanner para liberar recursos
     }
 
 }
